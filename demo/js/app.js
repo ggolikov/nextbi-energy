d8e9c043b3f44488394e611b11532c618d528444
ggolikov <gr.golikov@gmail.com>
2018-04-12 18:32:56 +0300

    Update leaflet and webpack

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,10 +1,11 @@
-var L = global.L || require('leaflet');
-require('../../index.js');
+import Plugin from '../../index.js';
 
-var osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+const osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }),
     point = L.latLng([55.819723, 37.611661]),
     map = new L.Map('map', {layers: [osm], center: point, zoom: 17, maxZoom: 22}),
     root = document.getElementById('content');
+
+console.log(Plugin);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-require('./src/L.Plugin.js');
+import Plugin from './src/L.Plugin';
 
+export default Plugin;
 // module.exports = L.Plugin;

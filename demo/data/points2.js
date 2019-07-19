30164e6f1a59cb682f9fbfe4f8cf90956590895b
Grigory Golikov <golikov@nextbi.ru>
2019-07-19 02:53:45 -0700

    Add actions

---
+++ demo/data/points2.js
@@ -0,0 +1,27 @@
+const points2 = {
+    "type":"FeatureCollection",
+    "features":[
+        {"type":"Feature","properties":{"name":"Сергиев Посад",     "capacity":35,   "risk":100},"geometry":{"type":"Point","coordinates":[38.12908172607422,56.30882440849725]}},
+        {"type":"Feature","properties":{"name":"Хотьково",          "capacity":20,   "risk":50},"geometry":{"type":"Point","coordinates":[37.98849105834961,56.255271382542325]}},
+        {"type":"Feature","properties":{"name":"Абрамцево",         "capacity":5,    "risk":40},"geometry":{"type":"Point","coordinates":[37.96806335449219,56.22235913987599]}},
+        {"type":"Feature","properties":{"name":"Лоза",              "capacity":5,    "risk":100},"geometry":{"type":"Point","coordinates":[38.21319580078125,56.25918081245906]}},
+        {"type":"Feature","properties":{"name":"Пересвет",          "capacity":20,   "risk":50},"geometry":{"type":"Point","coordinates":[38.17543029785156,56.416370288708414]}},
+        {"type":"Feature","properties":{"name":"Скоропусковский",   "capacity":10,   "risk":70},"geometry":{"type":"Point","coordinates":[38.14126968383789,56.36677142898368]}},
+        {"type":"Feature","properties":{"name":"Краснозаводск",     "capacity":20,   "risk":40},"geometry":{"type":"Point","coordinates":[38.24409484863281,56.43820369358165]}},
+        {"type":"Feature","properties":{"name":"Васильевское",      "capacity":10,   "risk":10},"geometry":{"type":"Point","coordinates":[37.89665222167969,56.34432621229825]}},
+        {"type":"Feature","properties":{"name":"Богородское",       "capacity":20,   "risk":30},"geometry":{"type":"Point","coordinates":[38.17920684814453,56.49339572586091]}},
+        {"type":"Feature","properties":{"name":"Реммаш",            "capacity":10,   "risk":30},"geometry":{"type":"Point","coordinates":[38.09886932373046,56.45224638739494]}},
+        {"type":"Feature","properties":{"name":"Мостовик",          "capacity":10,   "risk":40},"geometry":{"type":"Point","coordinates":[37.935447692871094,56.30549144544153]}},
+        {"type":"Feature","properties":{"name":"Бужаниново",        "capacity":10,   "risk":20},"geometry":{"type":"Point","coordinates":[38.29936981201172,56.392909148568776]}},
+        {"type":"Feature","properties":{"name":"Шеметово",          "capacity":5,    "risk":0},"geometry":{"type":"Point","coordinates":[38.07792663574219,56.52919914018469]}},
+        {"type":"Feature","properties":{"name":"Константиново",     "capacity":5,    "risk":20},"geometry":{"type":"Point","coordinates":[38.03535461425781,56.55304929997021]}},
+        {"type":"Feature","properties":{"name":"Торгашино",         "capacity":5,    "risk":20},"geometry":{"type":"Point","coordinates":[38.2049560546875,56.635840174256764]}},
+        {"type":"Feature","properties":{"name":"Семхоз",            "capacity":5,    "risk":70},"geometry":{"type":"Point","coordinates":[38.07432174682617,56.28910808646475]}},
+        {"type":"Feature","properties":{"name":"Федорцово",         "capacity":5,    "risk":20},"geometry":{"type":"Point","coordinates":[38.14796447753906,56.67207490184347]}},
+        {"type":"Feature","properties":{"name":"Шабурново",         "capacity":5,    "risk":20},"geometry":{"type":"Point","coordinates":[37.92308807373046,56.547750562697274]}},
+        {"type":"Feature","properties":{"name":"Озерецкое",         "capacity":5,    "risk":10},"geometry":{"type":"Point","coordinates":[37.83159255981445,56.3101575122987]}},
+        {"type":"Feature","properties":{"name":"Зубцово",           "capacity":5,    "risk":100},"geometry":{"type":"Point","coordinates":[38.144874572753906,56.197919018820286]}},
+    ]
+};
+
+export default points2;
--- demo/js/app.js
+++ demo/js/app.js
@@ -1,8 +1,13 @@
 import L from 'leaflet';
 import points from '../data/points';
+import points2 from '../data/points2';
 import lines from '../data/lines';
+import palette from './palette';
 
-let osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+// let osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+// let osm = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+let osm = L.tileLayer('http://api.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}{r}.png?access_token=pk.eyJ1Ijoia29zbW9zbmlta2lydSIsImEiOiJjaWhxMHNlZDgwNGFldHBtMjdyejQ3YTJ3In0.3UAAWcIBabrbUhHwmp1WjA', {
+// let osm = L.tileLayer('http://atilecart.kosmosnimki.ru/kosmo/{z}/{x}/{y}{r}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }),
@@ -16,7 +21,6 @@ lmap.fitBounds([
 
 let geojsonMarkerOptions = {
     radius: 8,
-    fillColor: "#ff7800",
     color: "#000",
     weight: 1,
     opacity: 1,
@@ -31,30 +35,45 @@ let greenHex = "0x00ff00",
 
 console.log(greenInt);
 console.log(redInt);
-let linesLayer = new L.geoJSON(lines).addTo(lmap);
-
-let pointsLayer = new L.geoJSON(points, {
-    pointToLayer: function (feature, latlng) {
-        let radius = feature.properties.capacity,
-            colorInt = greenInt + feature.properties.risk * (redInt - greenInt),
-            colorHex = numHex(colorInt);
-
-            console.log(colorHex);
-
-        return L.circleMarker(latlng, {...geojsonMarkerOptions, radius, fillColor: `#${colorHex}` });
-    },
-    onEachFeature: function(feature, layer){
-        layer.bindPopup(feature.properties.city);
+let linesLayer = new L.geoJSON(lines, {
+    style: function(feature) {
+        return { color: `#999999`, weight: 2 }
     },
 }).addTo(lmap);
 
-function numHex(s)
-{
-    var a = s.toString(16);
-    if ((a.length % 2) > 0) {
-        a = "0" + a;
-    }
-    return a;
+let pointsLayer;
+
+function addPoints(points) {
+    if (pointsLayer && lmap.hasLayer(pointsLayer)) {lmap.removeLayer(pointsLayer)};
+    pointsLayer = new L.geoJSON(points, {
+        style: function(feature) {
+            let { name, risk } = feature.properties;
+            return { fillColor: palette[risk], fillOpacity: 1 }
+        },
+        pointToLayer: function (feature, latlng) {
+            let radius = feature.properties.capacity;
+
+            return L.circleMarker(latlng, {...geojsonMarkerOptions, radius })
+        },
+        onEachFeature: function(feature, layer){
+        },
+    }).addTo(lmap);
 }
 
+addPoints(points);
+
 window.lmap = lmap;
+
+window.setRisk = () => {
+    addPoints(points2);
+}
+
+window.resetRisk = () => {
+    addPoints(points);
+}
+
+let setButton = document.getElementById('set');
+let resetButton = document.getElementById('reset');
+
+setButton.addEventListener('click', window.setRisk);
+resetButton.addEventListener('click', window.resetRisk);
